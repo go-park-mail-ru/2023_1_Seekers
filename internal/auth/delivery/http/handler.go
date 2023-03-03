25d957d1092bb405b206afb0ae27ae6e1bfccd57
dyndtikj <vlamaster08@gmail.com>
2023-03-03 22:15:06 +0300

    same site

--- internal/auth/delivery/http/handler.go
+++ internal/auth/delivery/http/handler.go
@@ -178,6 +178,7 @@ func (h *handlers) SignIn(w http.ResponseWriter, r *http.Request) {
 		Expires:  time.Now().Add(config.CookieTTL),
 		Path:     config.CookiePath,
 		HttpOnly: true,
+		SameSite: http.SameSiteNoneMode,
 	})
 	pkg.SendJSON(w, http.StatusOK, user)
 }
