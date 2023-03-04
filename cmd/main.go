1b0e28cdd265cb61d70cecb847c176960df4bc50
dyndtikj <vlamaster08@gmail.com>
2023-03-04 10:45:20 +0300

    Deploy (cookie fix test)

--- cmd/main.go
+++ cmd/main.go
@@ -57,7 +57,8 @@ func main() {
 	}
 
 	log.Info("server started")
-	err := server.ListenAndServeTLS("localhost.crt", "localhost.key")
+	err := server.ListenAndServe()
+	//err := server.ListenAndServeTLS("localhost.crt", "localhost.key")
 	if err != nil {
 		log.Errorf("server stopped %v", err)
 	}
