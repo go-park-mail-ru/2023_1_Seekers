8b177dd69c714aeb4dd95eafbc490f2a2879a5d4
dyndtikj <vlamaster08@gmail.com>
2023-05-31 13:17:55 +0300

    smtp

--- pkg/json/escape.go
+++ pkg/json/escape.go
@@ -1,11 +1,8 @@
 package json
 
-import (
-	"strings"
-)
-
 func Escape(i string) (string, error) {
-	return strings.ReplaceAll(i, "\"", "\\\""), nil
+	return i, nil
+	//return strings.ReplaceAll(i, "\"", "\\\""), nil
 	//b, err := json.Marshal(i)
 	//if err != nil {
 	//	return "", err
