bdb46ba3d4970613831aa5609648fde4a4c72ef5
maksim.vlasov <maksim.vlasov@vk.team>
2023-06-19 17:24:16 +0300

    fix schema access

--- scripts/setup_user_sql/000_setup_users.sql
+++ scripts/setup_user_sql/000_setup_users.sql
@@ -3,6 +3,9 @@ SET search_path TO mail, public;
 CREATE USER mail_service_user WITH PASSWORD :db_mail_service_user_pw;
 CREATE USER user_service_user WITH PASSWORD :db_user_service_user_pw;
 
+GRANT USAGE ON SCHEMA mail TO mail_service_user;
+GRANT USAGE ON SCHEMA mail TO user_service_user;
+
 GRANT SELECT, UPDATE, INSERT, DELETE, TRIGGER ON folders, boxes TO mail_service_user;
 GRANT SELECT, UPDATE, INSERT ON messages, attaches TO mail_service_user;
 
